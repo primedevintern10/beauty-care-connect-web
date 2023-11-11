5a2db1144744261f1a45b7805071b7a9371c3ae9
Weerasinghe <neranjan@techbsl.com>
2023-11-11 08:32:57 +0530

    first

--- my-app/app/posts/new/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>NEW POSTS</div>
-  )
-}
-
-export default page
--- my-app/app/user/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>Hello User</div>
-  )
-}
-
-export default page
