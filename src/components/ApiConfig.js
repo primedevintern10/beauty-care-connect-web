63d68e42e0f513d1c989c57b9367d79ebefb5a9c
primedevintern10 <primedevintern10@gmail.com>
2026-03-20 07:52:24 +0530

    fix: update APIURL definition to include fallback for production environment

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,3 +1,4 @@
-const APIURL = process.env.REACT_APP_APP_URL
+const APIURL =
+  process.env.REACT_APP_APP_URL || 'https://beauty-care-connect-api-production.up.railway.app/'
 
 export default APIURL
